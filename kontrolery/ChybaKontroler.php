f21a6027a8645bfbde8f36b86b929dfe6b49b29e
Roman3349 <ondracek.roman@centrum.cz>
2015-07-10 21:14:07 +0200

    Oprava kontroléru pro generování chybové stránky

--- kontrolery/ChybaKontroler.php
+++ kontrolery/ChybaKontroler.php
@@ -6,7 +6,7 @@ class ChybaKontroler extends Kontroler {
 
     public function zpracuj($parametry) {
         // Nastavení hlavičky požadavku
-        header('HTTP/1.0 404 Not Found');
+        header($_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
         $this->hlavicka['titulek'] = 'Chyba 404';
         $this->sablona = 'chyba';
     }
